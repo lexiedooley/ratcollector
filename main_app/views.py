ccecfd2e24925d165658da78e1a3ab5d35e3ae10
lexiedooley <lexiedools@gmail.com>
2023-07-10 16:54:16 -0400

    finished hw

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+rats = [
+  {'name': 'Pixie', 'breed': 'Feeder rat', 'description': 'sweet, loving and a runt', 'age': 1},
+  {'name': 'Ralphie', 'breed': 'Rex', 'description': 'confused, social and nosey', 'age': 1.5},
+]
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def rats_index(request):
+    return render(request, 'rats/index.html', {
+        'rats': rats
+    })
